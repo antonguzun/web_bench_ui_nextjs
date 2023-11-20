be4f68a56b0623a8e79957163604b7e8e7d06eaf
antonguzun <aguzun@ibitcy.com>
2023-11-20 16:08:25 +0200

    refactoring

---
+++ .eslintrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', "next/core-web-vitals", "plugin:prettier/recommended"],
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint', "prettier"],
+    rules: {
+        "prettier/prettier": "error"
+    },
+    root: true,
+};
