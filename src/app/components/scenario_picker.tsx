1d1f532bcf404658692b34303196477730ac5a99
antonguzun <ayuguzun@gmail.com>
2023-11-20 23:35:30 +0200

    add chart

--- src/app/components/scenario_picker.tsx
+++ src/app/components/scenario_picker.tsx
@@ -8,17 +8,23 @@ export default function ScenarioSet({
   setTestName: React.Dispatch<React.SetStateAction<string>>;
 }) {
   return (
-    <div className="flex flex-row pb-2 pt-2">
-      {Array.from(uniqueTestNames).map((tn: string) => (
-        <div className="flex-auto pb-2" key={tn}>
-          <button
-            className="h-14 bg-slate-700 mr-4 pr-6 pl-6 rounded hover:bg-slate-500 active:bg-slate-500 focus:ring focus:ring-black-300"
-            onClick={() => setTestName(tn)}
-          >
-            {tn}
-          </button>
-        </div>
-      ))}
+    <div>
+      Scenarios:
+      <ul>
+
+        {Array.from(uniqueTestNames).map((tn: string) => (
+          <div className="pb-1" key={tn}>
+            <li
+              className="bg-slate-700 rounded-md hover:bg-slate-500 active:bg-blue-600 focus:ring-2 focus:ring-slate-300"
+              onClick={() => setTestName(tn)}
+            >
+              <div className="pl-3">{tn}</div>
+            </li>
+
+          </div>
+        ))}
+      </ul>
+
     </div>
   );
 }
