f407667d9330260c09028886d7f9076757455ea5
antonguzun <aguzun@ibitcy.com>
2023-11-20 17:42:43 +0200

    add orm filter

--- src/app/entities/filter.ts
+++ src/app/entities/filter.ts
@@ -1,15 +1,5 @@
-export class Filter {
-  testName: string;
-  webserverName: string | null;
-  language: string | null;
-  database: string | null;
-  orm: string | null;
-
-  constructor(testName: string) {
-    this.testName = testName;
-    this.webserverName = null;
-    this.language = null;
-    this.database = null;
-    this.orm = null;
-  }
+export enum OrmOption {
+  All = "0",
+  UseOrm = "1",
+  WithoutOrm = "2",
 }
