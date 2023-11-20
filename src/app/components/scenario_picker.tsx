4c716fb87b18f2c10c16c3bad2bc48dff99fd92b
antonguzun <aguzun@ibitcy.com>
2023-11-20 18:28:55 +0200

    refactoring

--- src/app/components/scenario_picker.tsx
+++ src/app/components/scenario_picker.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-export default function TestNamesSet({
+export default function ScenarioSet({
   uniqueTestNames,
   setTestName,
 }: {
@@ -8,11 +8,11 @@ export default function TestNamesSet({
   setTestName: React.Dispatch<React.SetStateAction<string>>;
 }) {
   return (
-    <div className="flex flex-row pb-3 pt-3">
+    <div className="flex flex-row pb-2 pt-2">
       {Array.from(uniqueTestNames).map((tn: string) => (
-        <div className="flex-auto pb-3" key={tn}>
+        <div className="flex-auto pb-2" key={tn}>
           <button
-            className="h-14 bg-gradient-to-r from-cyan-700 to-blue-700 mr-4 pr-6 pl-6 rounded hover:bg-violet-600 active:bg-violet-700 focus:ring focus:ring-black-300"
+            className="h-14 bg-slate-700 mr-4 pr-6 pl-6 rounded hover:bg-slate-500 active:bg-slate-500 focus:ring focus:ring-black-300"
             onClick={() => setTestName(tn)}
           >
             {tn}
