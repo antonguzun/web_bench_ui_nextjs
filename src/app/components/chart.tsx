4f7bcd83dda1c97182275980ed335a5b5e76d03c
antonguzun <ayuguzun@gmail.com>
2023-12-02 01:03:49 +0200

    fixes

--- src/app/components/chart.tsx
+++ src/app/components/chart.tsx
@@ -4,26 +4,30 @@ import React from 'react'
 import { BarChart, Bar, XAxis, YAxis, ResponsiveContainer, CartesianGrid, Cell } from 'recharts'
 import { Report } from '../entities/report'
 
-export default function ReportChart({ data, hoverRow, setHoverRow}: { data: Report[], hoverRow: number ,       setHoverRow: React.Dispatch<React.SetStateAction<number>>
-}) {
+export default function ReportChart(
+  { data, hoverRow, setHoverRow }:
+    {
+      data: Report[],
+      hoverRow: number,
+      setHoverRow: React.Dispatch<React.SetStateAction<number>>
+    }) {
   return (
     <>
       <ResponsiveContainer width="100%" height="100%">
         <BarChart width={100} height={100} data={data}>
-          <CartesianGrid strokeDasharray="4 4" fill="black" vertical={false} fillOpacity={0.1}/>
+          <CartesianGrid strokeDasharray="4 4" fill="black" vertical={false} fillOpacity={0.1} />
           <XAxis dataKey="webserverName" angle={-30} height={75}
             textAnchor="end" tick={{ fill: 'white' }} />
           <YAxis height={70} tick={{ fill: 'white' }} />
           <Bar isAnimationActive={false} dataKey="requestsPerSecond">
-          {
-            data.map((entry, index) => (
-              <Cell key={`cell-${entry.id}`} fill={entry.id === hoverRow ? "#94a3b8" : "#455569"}
-              onMouseEnter={() => setHoverRow(entry.id)}
-              onMouseLeave={() => setHoverRow(-1)}
-  
-              />
-            ))
-          }
+            {
+              data.map((entry, _) => (
+                <Cell key={`cell-${entry.id}`} fill={entry.id === hoverRow ? "#94a3b8" : "#455569"}
+                  onMouseEnter={() => setHoverRow(entry.id)}
+                  onMouseLeave={() => setHoverRow(-1)}
+                />
+              ))
+            }
           </Bar>
         </BarChart>
       </ResponsiveContainer>
